c344d9d958d9487288973a3745c70ec5d79362e3
Sabur7264 <49846108+Sabur7264@users.noreply.github.com>
2020-07-17 15:54:15 +0600

    woodCalculator Added

--- assignment.js
+++ assignment.js
@@ -14,3 +14,24 @@ function feetToMile(feet){
 }
 let feetResult = feetToMile(5056); // Feet Number
 console.log(feetResult);
+
+
+// 2) Wood Calculator
+function woodCalculator(chair , table , bed){
+    if(chair < 0 || table < 0 || bed < 0){
+        return "input items";
+    }
+    else if(chair == null || table == null || bed == null){
+        return "you must be use three vlaue";
+    }
+    else{
+        var woodChair = chair * 1 ;
+        var woodTable = table * 3 ;
+        var woodBed = bed * 5 ;
+        var wood = woodChair + woodTable + woodBed ;
+        var result = "You have to need " +wood + " cubic feet \n";
+        return result;
+    }
+}
+let woodResult = woodCalculator(16, 8, 4); // Chair, Table, Bed
+console.log(woodResult);
